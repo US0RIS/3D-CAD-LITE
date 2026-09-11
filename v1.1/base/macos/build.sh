28a7ea45879b7d0b05084ee8243864db52cd143e
US0RIS <emmett.dubeck@icloud.com>
2026-09-11 13:50:56 -0700

    Test physical-verification API during macOS qualification

--- v1.1/base/macos/build.sh
+++ v1.1/base/macos/build.sh
@@ -20,6 +20,7 @@ python acceptance_test.py
 python acceptance_api_test.py
 python component_fidelity_test.py
 python production_readiness_test.py
+python production_readiness_api_test.py
 python v1_test.py
 python release_test.py
 python -m PyInstaller --noconfirm --clean forgecad.spec
